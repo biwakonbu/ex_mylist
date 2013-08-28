2fd4fd8caf305f594c9ad45e77adf03a56ec37d6
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2013-08-28 19:58:26 +0900

    omniauth twitter

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+class SessionsController < ApplicationController
+  def create
+    auth = request.env["omniauth.auth"]
+    user = User.find_by_provider_and_uid(auth["provider"],
+                                         auth["uid"]) ||
+      User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+
+    Twitter.configure do |config|
+      config.oauth_token = auth["credentials"]["token"]
+      config.oauth_token_secret = auth["credentials"]["secret"]
+    end
+
+    redirect_to '/contents/buttons', :notice => "認証しました!!"
+  end
+
+  def destroy
+    session[:user_id] = ni;
+    redirect_to '/', :notice => "認証を外しました"
+  end
+end
