9546eb2290a62139e86766690c66c39a0a506c38
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2013-08-28 17:45:21 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExMylist::Application.config.secret_token = '3027eafab7ff9f2b7150210a74a9c3345c382aa58f24925f75cedfa76440ee15141c9c22151de5c680b1a43c1c4415ce8feedcd47ff7c12d391b9190b61050c4'
