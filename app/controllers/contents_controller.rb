b34a2882cbbc24c93aa8a50ffd6771b0643689d5
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2013-08-29 13:46:29 +0900

    to_integer session[:user_id]

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -3,7 +3,7 @@ class ContentsController < ApplicationController
   def index
     if session[:user_id]
       @user = User.find_by_uid(session[:twitter_id])
-      @movie = Movie.find(:all, :conditions => {:user => session[:user_id]})
+      @movie = Movie.find(:all, :conditions => {:user => session[:user_id].to_i})
       @url = "http://www.nicovideo.jp/watch/"
     end
   end
@@ -20,8 +20,3 @@ class ContentsController < ApplicationController
     redirect_to '/contents/index'
   end
 end
-
-
-
-
-
